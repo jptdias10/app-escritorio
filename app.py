93dd42ccff252ab5acce1b55cdf861e90c054bad
Joao Dias <64169878+jptdias10@users.noreply.github.com>
2023-02-25 15:14:27 -0300

    feat: get dataframe from sheet

--- app.py
+++ app.py
@@ -6,3 +6,8 @@ from typing import Dict, List
 file = 'Caixa.xlsm'
 workbook = openpyxl.load_workbook(file)
 sheets = workbook.sheetnames
+#TODO loop para ler página a página
+#TODO levando em conta que há 2 modelos, faça uma função para identificar pela data se é do formato antigo ou novo
+sheet = workbook['MARÇO-16'].values
+pag_toda = pd.DataFrame(sheet)
+pag_toda.replace({None:np.nan}, inplace=True)
