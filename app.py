e7ccda67e90164c942b905a38eadfb1cf28079e4
Joao Dias <64169878+jptdias10@users.noreply.github.com>
2023-02-25 15:15:03 -0300

    feat: first functions

--- app.py
+++ app.py
@@ -3,6 +3,55 @@ import numpy as np
 import openpyxl
 from tqdm import tqdm
 from typing import Dict, List
+
+def remove_none_rows_and_cols(df):
+    df = df.dropna(axis=1, how='all')
+    df = df.dropna(axis=0, how='all')
+    return df
+
+def get_ixinit_entradas_saidas(df:pd.DataFrame):
+    """Retorna os índices dos inícios das tabelas 'Entradas' e 'Saídas'
+
+    Args:
+        df (pd.DataFrame): _description_
+
+    Returns:
+        List[index]: Indícies dos inícios das tabelas 'Entradas' e 'Saídas'
+    """
+    if (df[6].str.contains('ENTRADAS') == True).any():
+        ixinit_entrada = (df[6].str.contains('ENTRADAS') == True).idxmax()
+    else:
+        ixinit_entrada = None
+    if (df[6].str.contains('SAÍDAS') == True).any():
+        ixinit_saidas = (df[6].str.contains('SAÍDAS') == True).idxmax()
+    else:
+        ixinit_saidas = None
+    return [ixinit_entrada,ixinit_saidas]
+
+def get_entradas_saidas(df:pd.DataFrame)->List[pd.DataFrame]:
+    """Separa duas tabelas com os índices de início já identificados
+
+    Args:
+        df (pd.DataFrame):
+        ixinits (List[int]):
+
+    Returns:
+        List[pd.DataFrame]:
+    """
+    df = remove_none_rows_and_cols(df)
+    ixinits = get_ixinit_entradas_saidas(df)
+    entradas = df.iloc[ixinits[0]:ixinits[1]-2] #TODO Função get_entradas que já retorna a tabela entradas toda tratada
+    saidas = df.iloc[ixinits[1]-2:] #TODO Função get_saidas que já retorna a tabela saidas toda tratada
+    return [entradas, saidas]
+
+def get_fechamento(df):
+    #TODO Função get_fechamento que já retorna a tabela fechamento toda tratada
+    return remove_none_rows_and_cols(df)
+
+def get_adiantamento(df):
+    #TODO Função get_adiantamento que já retorna a tabela adiantamento toda tratada
+    return remove_none_rows_and_cols(df)
+
 file = 'Caixa.xlsm'
 workbook = openpyxl.load_workbook(file)
 sheets = workbook.sheetnames
